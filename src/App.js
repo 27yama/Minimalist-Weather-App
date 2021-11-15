eb14d5546f9b3ea04f0fabef0fac780d988d7b29
27yama <dlwnsgur109@gmail.com>
2021-11-14 20:00:25 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import react, {useState} from "react";
+
+const API = {
+  key: "KEY",
+  baseurl: "BASEURL"
+}
 
 function App() {
+
+  /*
+  const createDate = (dt) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[dt.getDay()];
+    let date = dt.getDate();
+    let month = months[dt.getMonth()];
+    let year = dt.getFullYear();
+    return `${day}, ${date}, ${month}, ${year}`
+  }
+  */
+
+  const [query, setQuery] = useState('');
+  const [weather, setweather] = useState({});
+
+  const search = (evnt) => {
+    if (evnt.key === "Enter"){
+      fetch(`${API.baseurl}weather?q=${query}&units=metric&APPID=${API.key}`)
+      .then(result => result.json())
+      .then(returned => {
+        setweather(returned);
+        setQuery("");
+        console.log(returned);
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.weather[0].main == "Clear" ? ('AppClear') : (weather.weather[0].main == "Clouds" ? ('AppClouds') : (weather.weather[0].main == "Mist" ? ('AppMist') : weather.weather[0].main == "Snow" ? ('AppSnow') : weather.weather[0].main == "Smoke" ? ('AppSmoke') : (weather.weather[0].main == "Fog" ? ('AppFog') : weather.weather[0].main == "Rain" ? ('AppRain') : 'App'))))) : 'AppNotFound'}>
+      <main>
+        <div className="search">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder = "Countries, Cities, Towns"
+            onChange = {qr => setQuery(qr.target.value)}
+            value = {query}
+            onKeyPress = {search}
+            />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+        <div className="location-bar">
+          <div className="location">{weather.name}, {weather.sys.country}</div>
+          <div className="date">{new Date( new Date().getTime() + weather.timezone * 1000).toUTCString().replace( / GMT$/, "" )}</div>
+        </div>
+        <div className="weather-bar">
+          <div className="temperature">
+            {Math.round(weather.main.temp)}°c
+          </div>
+          <div className="weatherStatus"> {weather.weather[0].main} </div>
+            </div>
+        </div>
+        ) : (<div className="NotFound"> Location Not Found </div>)}
+      </main>
     </div>
   );
 }
